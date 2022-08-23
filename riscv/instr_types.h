ef7321d4402ff460b3966d325492b2886f5e845f
saintanger589 <gauravsri589@gmail.com>
2022-08-23 09:48:37 -0700

    riscv coding start

---
+++ riscv/instr_types.h
@@ -0,0 +1,12 @@
+class I_type_instr{
+  //addi
+
+};
+
+class R_type_instr{
+
+};
+
+class U_type_instr{
+
+};
---
+++ riscv/simulate.cpp
@@ -0,0 +1,13 @@
+//this file is to simulate the instructions
+//of riscv to understand various instrauctions
+
+//integer register immediate instrauctions
+#include <stdint.h>
+#include <stdio.h>
+#include <iostream.h>
+#include "riscv_instr.h"
+
+int main(){
+  riscv_instr instr;
+  
+}
